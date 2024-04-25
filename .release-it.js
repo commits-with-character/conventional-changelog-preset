7ed4b6691576d244cd2988c0fbc2f73441cb1869
Will Stone <654103+will-stone@users.noreply.github.com>
2024-04-25 12:08:55 +0100

    ~ Another dummy commit

--- .release-it.js
+++ .release-it.js
@@ -4,9 +4,9 @@ module.exports = {
   git: {
     commitMessage: "Release v${version}",
   },
-  github: {
-    release: true,
-  },
+  // github: {
+  //   release: true,
+  // },
   plugins: {
     "@release-it/conventional-changelog": {
       infile: "CHANGELOG.md",
