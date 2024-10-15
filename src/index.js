d093695081b97a347c329ad253431a6bf68b1565
Your Name <you@example.com>
2024-10-15 12:57:44 +0100

    Remove commented-out code

--- src/index.js
+++ src/index.js
@@ -1,25 +1,9 @@
 const { createParserOpts } = require('./parser.js')
 const { createWriterOpts } = require('./writer.js')
-// const {
-//   createConventionalChangelogOpts,
-// } = require('./conventional-changelog.js')
-// const {
-//   createConventionalRecommendedBumpOpts,
-// } = require('./conventional-recommended-bump.js')
 const { whatBump } = require('./what-bump.js')
 
 function createPreset() {
-  // const recommendedBumpOpts = createConventionalRecommendedBumpOpts(parserOpts)
-  // const conventionalChangelog = createConventionalChangelogOpts(
-  //   parserOpts,
-  //   writerOpts,
-  // )
-
   return {
-    // conventionalChangelog,
-    // parserOpts,
-    // recommendedBumpOpts,
-    // writerOpts,
     parser: createParserOpts(),
     whatBump,
     writer: createWriterOpts(),
