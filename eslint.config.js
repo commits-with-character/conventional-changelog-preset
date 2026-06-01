e1692df8d726514449b94999e354501604326eeb
Will Stone <654103+will-stone@users.noreply.github.com>
2026-06-01 20:35:15 +0100

    Upgrade dev deps

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,7 @@
 import config from '@will-stone/eslint-config'
 
 export default [
-  ...(await config()),
+  ...(await config({ cwd: import.meta.dirname })),
   {
     rules: {
       'unicorn/prefer-module': 'off',
